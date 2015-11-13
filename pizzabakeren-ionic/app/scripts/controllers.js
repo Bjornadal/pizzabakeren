79b8c3a839550a1ac36f6dfd5f12381d5312815b
Andreas Bjørnådal <bjornadal@gmail.com>
2015-11-13 13:38:27 +0100

    Misc + added icon

--- pizzabakeren-ionic/app/scripts/controllers.js
+++ pizzabakeren-ionic/app/scripts/controllers.js
@@ -1,6 +1,17 @@
 angular.module('starter.controllers', [])
 
-  .controller('DashCtrl', function ($scope) {
+  .controller('DashCtrl', function ($scope, $localstorage, $cordovaToast, $state) {
+    $scope.startOrder = function() {
+      var settings = $localstorage.getObject('settings');
+      if (!settings.username || !settings.group) {
+        $cordovaToast
+          .show('Du må legge inn navn og gruppe før du kan bestille', 'long', 'center')
+          .then(function(success) {}, function (error) {});
+      }
+      else {
+        $state.go('tab.pizza-select');
+      }
+    };
   })
 
   .controller('PizzaSelectCtrl', function ($scope, $firebaseObject, ENV, $location, $state, OrderFactory) {
@@ -41,11 +52,7 @@ angular.module('starter.controllers', [])
 
       $cordovaToast
         .show('Bestillingen ble sendt!', 'long', 'center')
-        .then(function(success) {
-          // success
-        }, function (error) {
-          // error
-        });
+        .then(function(success) {}, function (error) {});
     }
   })
 
@@ -55,20 +62,19 @@ angular.module('starter.controllers', [])
     // To listen for when this page is active (for example, to refresh data),
     // listen for the $ionicView.enter event:
     //
-    //$scope.$on('$ionicView.enter', function(e) {
-    //});
+    $scope.$on('$ionicView.enter', function(e) {
+      $scope.settings = $localstorage.getObject('settings');
+      var ref = new Firebase(ENV.apiEndpoint + "/orders/" + $scope.settings.group + "/" + moment().format("YYYY-MM-DD"));
+      $scope.orders = $firebaseArray(ref);
 
-    $scope.settings = $localstorage.getObject('settings');
-    var ref = new Firebase(ENV.apiEndpoint + "/orders/" + $scope.settings.group + "/" + moment().format("YYYY-MM-DD"));
-    $scope.orders = $firebaseArray(ref);
+      $scope.pizzaSummary = {};
+      $scope.sodaSummary = {};
 
-    $scope.pizzaSummary = {};
-    $scope.sodaSummary = {};
-
-    $scope.orders.$loaded(function() {
-      angular.forEach($scope.orders, function(value, key) {
-        $scope.pizzaSummary[value.pizzaNr] = ($scope.pizzaSummary[value.pizzaNr] == null) ? 1 : $scope.pizzaSummary[value.pizzaNr]+1;
-        $scope.sodaSummary[value.soda] = ($scope.sodaSummary[value.soda] == null) ? 1 : $scope.sodaSummary[value.soda]+1;
+      $scope.orders.$loaded(function() {
+        angular.forEach($scope.orders, function(value, key) {
+          $scope.pizzaSummary[value.pizzaNr] = ($scope.pizzaSummary[value.pizzaNr] == null) ? 1 : $scope.pizzaSummary[value.pizzaNr]+1;
+          $scope.sodaSummary[value.soda] = ($scope.sodaSummary[value.soda] == null) ? 1 : $scope.sodaSummary[value.soda]+1;
+        });
       });
     });
   })
@@ -77,11 +83,15 @@ angular.module('starter.controllers', [])
     $scope.chat = [{}];
   })
 
-  .controller('SettingsCtrl', function ($scope, $localstorage) {
+  .controller('SettingsCtrl', function ($scope, $localstorage, $cordovaToast) {
     $scope.settings = $localstorage.getObject('settings');
 
     $scope.save = function() {
       $scope.settings.group = $scope.settings.group.toLowerCase();
       $localstorage.setObject('settings', $scope.settings);
+
+      $cordovaToast
+        .show('Innstillinger ble lagret', 'long', 'center')
+        .then(function(success) {}, function (error) {});
     }
   });
