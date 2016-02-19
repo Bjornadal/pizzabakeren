d35dd9a94bf0f1556ef28929227a137237466cec
Andreas Bjørnådal <bjornadal@gmail.com>
2016-02-19 13:37:30 +0100

    Ready for push

--- app/js/app.js
+++ app/js/app.js
@@ -7,116 +7,130 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('pizzabakeren', ['ionic', 'firebase', 'config', 'ngCordova', 'angular.filter'])
 
-  .run(function ($ionicPlatform, $updateService) {
-    $ionicPlatform.ready(function () {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova) {
-        if (window.cordova.plugins && window.cordova.plugins.Keyboard) {
-          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-          cordova.plugins.Keyboard.disableScroll(true);
-        }
+    .run(function ($ionicPlatform, $updateService) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova) {
+                if (window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                    cordova.plugins.Keyboard.disableScroll(true);
+                }
 
-        $updateService.isUpdateAvailable().then(function (install) {
-          if (install) {
-            $updateService.installUpdate();
-          }
+                $updateService.isUpdateAvailable().then(function (install) {
+                    if (install) {
+                        $updateService.installUpdate();
+                    }
+                });
+            }
+
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+
+            // Push notifications
+            //$ionicPush.init({
+            //    "debug": true,
+            //    "onNotification": function(notification) {
+            //        var payload = notification.payload;
+            //        console.log(notification, payload);
+            //    },
+            //    "onRegister": function(data) {
+            //        console.log(data.token);
+            //    }
+            //});
+            //
+            //$ionicPush.register();
         });
-      }
+    })
 
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
-      }
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
+
+        // setup an abstract state for the tabs directive
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/menu/menu.html',
+                controller: 'AppCtrl'
+            })
+
+            // Each tab has its own nav history stack:
+
+            .state('app.order', {
+                url: '/order',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/order/startOrder.html',
+                        controller: 'OrderCtrl'
+                    }
+                }
+            })
+            .state('app.pizza-select', {
+                url: '/order/pizza',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/order/select-pizza.html',
+                        controller: 'PizzaSelectCtrl'
+                    }
+                }
+            })
+            .state('app.soda-select', {
+                url: '/order/pizza/soda',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/order/select-soda.html',
+                        controller: 'SodaSelectCtrl'
+                    }
+                }
+            })
+            .state('app.confirmation', {
+                url: '/order/pizza/soda/confirm',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/order/confirmation.html',
+                        controller: 'OrderConfirmationCtrl'
+                    }
+                }
+            })
+
+            .state('app.history', {
+                url: '/history',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/history/history.html',
+                        controller: 'HistoryCtrl'
+                    }
+                }
+            })
+
+            .state('app.settings', {
+                url: '/settings',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/settings/settings.html',
+                        controller: 'SettingsCtrl'
+                    }
+                }
+            })
+
+            .state('app.track', {
+                url: '/track',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/track/track.html',
+                        controller: 'TrackCtrl'
+                    }
+                }
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/order');
+
     });
-  })
-
-  .config(function ($stateProvider, $urlRouterProvider) {
-
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
-    $stateProvider
-
-    // setup an abstract state for the tabs directive
-      .state('app', {
-        url: '/app',
-        abstract: true,
-        templateUrl: 'templates/menu/menu.html',
-        controller: 'AppCtrl'
-      })
-
-      // Each tab has its own nav history stack:
-
-      .state('app.order', {
-        url: '/order',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/order/startOrder.html',
-            controller: 'OrderCtrl'
-          }
-        }
-      })
-      .state('app.pizza-select', {
-        url: '/order/pizza',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/order/select-pizza.html',
-            controller: 'PizzaSelectCtrl'
-          }
-        }
-      })
-      .state('app.soda-select', {
-        url: '/order/pizza/soda',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/order/select-soda.html',
-            controller: 'SodaSelectCtrl'
-          }
-        }
-      })
-      .state('app.confirmation', {
-        url: '/order/pizza/soda/confirm',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/order/confirmation.html',
-            controller: 'OrderConfirmationCtrl'
-          }
-        }
-      })
-
-      .state('app.history', {
-        url: '/history',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/history/history.html',
-            controller: 'HistoryCtrl'
-          }
-        }
-      })
-
-      .state('app.settings', {
-        url: '/settings',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/settings/settings.html',
-            controller: 'SettingsCtrl'
-          }
-        }
-      })
-
-    .state('app.track', {
-      url: '/track',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/track/track.html',
-          controller: 'TrackCtrl'
-        }
-      }
-    });
-
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/order');
-
-  });
